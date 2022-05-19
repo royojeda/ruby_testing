f093ab8ac2462069cf1383b2e30aee155619a8c0
Dennis Ojeda <royojeda1@gmail.com>
2022-05-19 21:24:11 +0800

    Finish 11b_cat_spec

--- spec/11b_cat_spec.rb
+++ spec/11b_cat_spec.rb
@@ -35,17 +35,20 @@ end
 
 describe Cat do
   # Create a subject with your choice of cat name and optional breed/color.
-
+  subject(:pepper) { described_class.new('Pepper', 'Siamese', 'Black') }
   # Write a test using the second shared_example to test that cat responds to
   # talk ('meow').
   context '' do
+    include_examples 'shared method name'
   end
 
   # remove the 'x' before running this test
-  xit 'is not hungry' do
+  it 'is not hungry' do
+    expect(pepper).not_to be_hungry
   end
 
   # remove the 'x' before running this test
-  xit 'is hiding' do
+  it 'is hiding' do
+    expect(pepper).to be_hiding
   end
 end
